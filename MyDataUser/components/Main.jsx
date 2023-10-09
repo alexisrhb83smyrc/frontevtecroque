e75e00e91e6d882d3c835e99659bfeeea5c3ca76
CassDeLaRy <mauriciomtz893@gmail.com>
2023-10-09 14:19:13 -0600

    [ADD] First part of Main component

--- MyDataUser/components/Main.jsx
+++ MyDataUser/components/Main.jsx
@@ -3,7 +3,25 @@ import '../style-sheets/Main.css'
 
 function Main() {
     return (  
-        
+        <div className="m-first-container">
+            <div className="m-add-data">
+                <p className="m-1_1">Add personal data</p>
+                <input
+                    className="mi m-1_2"
+                    placeholder="Nombre Completo"
+                ></input>
+                <input
+                    className="mi m-1_3"
+                    placeholder="Dirección"
+                ></input>
+                <input
+                    className="mi m-1_4"
+                    placeholder="Fecha de nacimiento"
+                ></input>
+            </div>
+
+
+        </div>
     );
 }
 
--- MyDataUser/pages/MyDataUserMain.jsx
+++ MyDataUser/pages/MyDataUserMain.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Main from "../components/Main"
 
 function MyDataUserMain() {
     return (
         <>
             <Header />
-
+            <Main />
             <Footer />
         </> 
     );
