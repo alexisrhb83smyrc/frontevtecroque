00a5152f22e7c876896d068f90ef272292cee3a2
CassDeLaRy <mauriciomtz893@gmail.com>
2023-10-09 13:55:14 -0600

    [ADD] Adding Header component

---
+++ MyDataUser/components/Header.jsx
@@ -0,0 +1,27 @@
+import '../style-sheets/Header.css'
+import { useNavigate } from "react-router-dom";
+import React from 'react';
+
+function Header() {
+
+    const navigate = useNavigate();
+
+    const regresarMenu = () => {
+        navigate("/");
+    }
+
+    return (
+        <header>
+            <div className="first-container">
+                <p className='f-c-1_1'>Welcome to</p>
+                <p className='f-c-1_2'>My Data User</p>
+                <p className='f-c-1_3'>Registration</p>
+            </div>
+            <div className="itemsp-regresarmenu">
+                <button className="botton-rm" onClick={regresarMenu}>REGRESAR AL MENÚ</button>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ MyDataUser/pages/MyDataUserMain.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+
+function MyDataUserMain() {
+    return ( 
+        <Header />
+
+    );
+}
+
+export default MyDataUserMain;
